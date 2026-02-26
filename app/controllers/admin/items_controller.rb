3577f52f3c572d21b1e9642d8eab076177b40785
ho.khanh.duong <ho.khanh.duong@sun-asterisk.com>
2026-02-26 10:11:10 +0700

    create feature sell hint to team

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -14,6 +14,40 @@ module Admin
       params
     end
 
+    def sell
+      item = Item.find(params[:item_id])
+      team = Team.find(params[:team_id])
+
+      # Check if team already has this item
+      if TeamItem.exists?(team_id: team.id, item_id: item.id)
+        redirect_back(fallback_location: admin_rooms_path, alert: "Team already has this item")
+        return
+      end
+
+      # Create TeamItem association
+      TeamItem.create!(
+        team: team,
+        item: item,
+        got_at: Time.current
+      )
+
+      # Add points to team if item has points
+      team.update!(current_points: team.current_points - item.points)
+
+      # Create point history record
+      PointHistory.create!(
+        team: team,
+        points: 0 - item.points,
+        description: "Sold item: #{item.name} - #{item.points} points"
+      )
+
+      redirect_back(fallback_location: admin_rooms_path, notice: "Item '#{item.name}' sold to #{team.name} successfully")
+    rescue ActiveRecord::RecordNotFound => e
+      redirect_back(fallback_location: admin_rooms_path, alert: "Item or team not found")
+    rescue StandardError => e
+      redirect_back(fallback_location: admin_rooms_path, alert: "Error selling item: #{e.message}")
+    end
+
     # See https://administrate-demo.herokuapp.com/customizing_controller_actions
     # for more information
   end
